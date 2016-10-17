613dfbff2114a71211164845dd9d181147e2240d
Spencer Zhang <sepmein@gmail.com>
2016-10-17 23:29:33 +0800

    added check email function

--- middlewares/parseBody.js
+++ middlewares/parseBody.js
@@ -4,14 +4,14 @@
 const parse = require('co-body');
 const is = require('is-js');
 
-module.exports = function* parseBody(next) {
-    try {
-        this.request.body = yield parse.json(this);
-    } catch (e) {
-        this.throw(400, e);
-    }
-    if (!is.empty(this.request.body)) {
-        this.assert(this.is('application/json'), 400, 'Only Accepts JSON');
-    }
-    yield next;
+module.exports = function * parseBody(next) {
+  try {
+    this.request.body = yield parse.json(this);
+  } catch (e) {
+    this.throw(400, e);
+  }
+  if (!is.empty(this.request.body)) {
+    this.assert(this.is('application/json'), 400, 'Only Accepts JSON');
+  }
+  yield next;
 };
